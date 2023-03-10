656c7c5c1c4bf49341b5ee70603df7c5aa151ea3
K-Krut <katiakrut448@gmail.com>
2023-03-10 02:48:58 +0200

    models

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
-# from .models import Product, ProductImage, Categories, AudioSpecification, Characteristics, Battery, Physical
 from .models import *
+
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+
+admin.site.register(Categories)
+admin.site.register(Color)
+admin.site.register(DriverType)
+admin.site.register(AudioSpecification)
+admin.site.register(Battery)
+admin.site.register(Physical)
+admin.site.register(Connectivity)
+admin.site.register(Controls)
+admin.site.register(Characteristics)
+admin.site.register(Types)
+admin.site.register(Product)
+admin.site.register(ProductImage)
--- store/models.py
+++ store/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
 class Categories(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
@@ -62,3 +65,45 @@ class Connectivity(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Characteristics(models.Model):
+    # product = models.ForeignKey("Product", on_delete=models.CASCADE, null=True)
+    audio = models.ForeignKey(AudioSpecification, on_delete=models.CASCADE, null=True)
+    battery = models.ForeignKey(Battery, on_delete=models.CASCADE, null=True)
+    physical = models.ForeignKey(Physical, on_delete=models.CASCADE, null=True)
+    connectivity = models.ForeignKey(Connectivity, on_delete=models.CASCADE, null=True)
+
+
+class Types(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, unique=True, db_index=True, null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=150, unique=True, db_index=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to=f"img/%Y/%m/%d/", blank=True)
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, null=True)
+    type = models.ForeignKey(Types, on_delete=models.CASCADE, null=True)
+    characteristics = models.ForeignKey(Characteristics, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'{self.id} --> {self.name}'
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    images = models.FileField("img", upload_to=f"img/%Y/%m/%d/")
+
+
+def __str__(self):
+    return self.product.name
