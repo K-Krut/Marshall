23b13a75e8c0149eca268f4672c3f7cc145f98e6
K-Krut <katiakrut448@gmail.com>
2023-04-30 19:47:18 +0300

    register

--- users/urls.py
+++ users/urls.py
@@ -7,20 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
     path('logout/', LogoutUser.as_view(), name='logout'),
-    path('register/', (RegisterUser.as_view()), name='register'),
+    path('register/', RegisterUser.as_view(), name='register'),
 ]
-
-"""
-
-    path('categories/', views.categories, name='categories'),  # HTTP response code: 200 (OK)
-    path('categories/<int:category_id>/', views.category_detail, name='category_detail'),  # HTTP response code: 200 (OK) or 404 (Not Found)
-    path('products/', views.products, name='products'),  # HTTP response code: 200 (OK)
-    path('products/<int:product_id>/', views.product_detail, name='product_detail'),  # HTTP response code: 200 (OK) or 404 (Not Found)
-    path('login/', views.login, name='login'),  # HTTP response code: 200 (OK) or 302 (Redirect)
-    path('logout/', views.logout, name='logout'),  # HTTP response code: 302 (Redirect)
-    path('register/', views.register, name='register'),  # HTTP response code: 200 (OK) or 302 (Redirect)
-    path('cart/', views.cart, name='cart'),  # HTTP response code: 200 (OK)
-    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),  # HTTP response code: 302 (Redirect)
-    path('cart/remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),  # HTTP response code: 302 (Redirect)
-    path('cart/clear/', views.clear_cart, name='clear_cart'),  # HTTP response code: 302 (Redirect)
-"""
