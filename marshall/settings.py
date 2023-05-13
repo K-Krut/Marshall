0884b5751ed68595e1270134f5ed765cc58c360c
K-Krut <katiakrut448@gmail.com>
2023-05-13 21:55:37 +0300

    login fixed

--- marshall/settings.py
+++ marshall/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -144,7 +143,7 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
-
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- users/views.py
+++ users/views.py
@@ -25,13 +25,16 @@ class LoginUser(DataMixin, SuccessMessageMixin, LoginView):
     form_class = LoginUserForm
     template_name = 'users/login.html'
     error_message = "Something went wrong"
-    success_url = reverse_lazy("")
+    success_url = reverse_lazy("home")
     user = ""
+    # next_page = '/'
+    # next = 'home'
     success_message = f"Successfully login"
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title="Login")
+        print(context)
         return dict(list(context.items()) + list(c_def.items()))
 
 
