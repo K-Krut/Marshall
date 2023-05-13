82279a51e21a4e789458ece9cdd8be91281bcb51
swiloper <myronishyn.ihor@icloud.com>
2023-05-13 22:21:50 +0300

    Added and displayed several products from the database

--- store/views.py
+++ store/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
+from .models import Product
 
 def index(request):
     return render(request, 'store/index.html')
 
 
-def category(request, category_slug):
-    return render(request, 'store/category_page.html')
+# def category(request, category_slug):
+#     return render(request, 'store/category_page.html')
 
 
-def categories(request):
-    return render(request, 'store/categories.html')
+def products(request):
+    queryset = Product.objects.all()
+    context = {
+        "products": queryset
+    }
+    return render(request, 'store/products.html', context)
 
 
-def product(request, product_slug):
-    return render(request, 'store/product.html')
+# def product(request, product_slug):
+#     return render(request, 'store/product.html')
