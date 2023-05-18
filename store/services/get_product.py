b53058ab8f673a635827332912a33ba28f3b14d7
K-Krut <katiakrut448@gmail.com>
2023-05-18 15:29:09 +0300

    product page

--- store/services/get_product.py
+++ store/services/get_product.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404
-from store.models import Product
+from store.models import Product, Characteristics
 
 
 def get_product_by_slug(slug):
@@ -8,3 +8,11 @@ def get_product_by_slug(slug):
         return get_object_or_404(Product, slug=slug)
     except Product.DoesNotExist:
         raise Http404(f"Product with slug {slug} not found...")
+
+
+def get_product_characteristics_by_slug(product):
+    try:
+        return Characteristics.objects.get(product=product)
+    except Product.DoesNotExist:
+        raise Http404(f"Characteristics for Product {product} not found...")
+
--- store/views.py
+++ store/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render
 from .models import Product
 from .models import Cart
 from .models import CartItem
+from .services.get_product import *
+
 
 def index(request):
     return render(request, 'store/index.html')
@@ -28,6 +30,48 @@ def products(request):
     return render(request, 'store/products.html', context)
 
 
+def product(request, product_slug):
+    context = {
+        "product": get_product_by_slug(product_slug),
+        "characteristics": get_product_characteristics_by_slug(get_product_by_slug(product_slug))
+    }
+    return render(request, 'store/product.html', context)
+
+
+"""
+def get_product_featuress(request, slug_id):
+    ip = get_client_ip(request)
+    content = {
+        "product": get_special_product(slug_id),
+        "cart": get_cart_by_user(ip),
+        "characteristic": get_characteristic_by_product(get_special_product(slug_id)),
+    }
+    return render(request, "store/get_product_featuress.html", context=content)
+
+
+
+def get_all_product_details(request, slug_id):
+
+    ip = get_client_ip(request)
+    liked = check_if_post_like_and_get_count(slug_id, request.user)
+
+    add_view_of_post(request, get_special_product(slug_id))
+    content = {
+        "product": get_special_product(slug_id),
+        "image": get_all_aditional_image_by_slug_id(slug_id),
+        "header_menu": get_header_menu(),
+        "special_menu_function": "All about the product",
+        "special_dict_menu": get_dict_aditional_like(
+            request.user, get_list_of_special(get_special_product(slug_id))
+        ),
+        "is_liked": liked,
+        "cart": get_cart_by_user(ip),
+    }
+
+    return render(request, "store/get_more.html", context=content)
+
+"""
+
 def cart(request):
     cart = None
     cartitems = []
@@ -38,16 +82,6 @@ def cart(request):
 
     context = {"cart": cart, "items": cartitems}
     return render(request, "store/cart.html", context)
-
-# def add_to_cart(request):
-#     data = json.loads(request.body)
-#     product_id = data["id"]
-#     product = Product.objects.get(id=product_id)
-#
-#     # if request.user.is_authenticated:
-#     cart, created = Cart.objects.get_or_create(completed=False)
-#     print(cart)
-#     return JsonResponse("WORK", safe=False)
 
 
 def add_to_cart(request):
@@ -62,6 +96,3 @@ def add_to_cart(request):
         cartitem.save()
 
     return JsonResponse(product_id, safe=False)
-
-# def product(request, product_slug):
-#     return render(request, 'store/product.html')
